20bda1f20c7f51b5f70166fbfbd1a45b8b7736f5
Naldo <edinaldoandradeoficial@gmail.com>
2026-09-21 00:36:26 -0300

    Update service worker to version 5

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - Fallback offline: serve index.html do cache
    ============================================================ */
 
-const CACHE_NAME = 'exna-music-v4';
+const CACHE_NAME = 'exna-music-v5';
 
 const CORE_ASSETS = [
   './',
